6e48a18b9908bd66a94d7b1b7cf6fd39db81f641
DanielPinnington <danielpinnington12@gmail.com>
2020-01-09 15:24:15 +0000

    Scene Templates & Scene Manager
    Scene templates and scene manager have been implemented, buttons on press segue from scene to scene, game scene will next be implemented.

---
+++ Arkanoid Game/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public void MenuBtn()
+    {
+        SceneManager.LoadScene("Start Menu"); //Loads Start Menu scene on Button Click
+    }
+
+    public void PlayBtn()
+    {
+        SceneManager.LoadScene("Level 1"); //Loads Level 1 scene on Button Click
+    }
+
+    public void ExitBtn()
+    {
+        SceneManager.LoadScene("Game Over"); //Loads Game Over scene on Button Click at the moment (Will be removed later)
+    }
+
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+}
