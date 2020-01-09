ed66b2a3774890051b3bbf80174912f406545919
DanielPinnington <danielpinnington12@gmail.com>
2020-01-09 21:25:12 +0000

    Bounce material and simple game over script
    Ball collides, bounces and triggers other collisions (paddle/game over collider) Game over collider yields 1 second before changing scene.

---
+++ Arkanoid Game/Assets/LoseCollider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour
+{
+    bool Collided; //Has it collided?
+
+    IEnumerator OnTriggerEnter2D(Collider2D collision) //IEnumator used for WaitForSeconds
+    {
+        Collided = true;
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("Game Over"); //Hard coded at the moment, change if needed.
+    }
+}
