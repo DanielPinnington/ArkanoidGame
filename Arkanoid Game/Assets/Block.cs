93429aae359bff5fa27b9ffc8971d0be7a769929
DanielPinnington <danielpinnington12@gmail.com>
2020-01-15 17:28:16 +0000

    Score system implemented
    Score system implemented in ScoreManager, when block is destroyed 50 points is displayed onto a UI Text box, updated everytime a block is destroyed.

--- Arkanoid Game/Assets/Block.cs
+++ Arkanoid Game/Assets/Block.cs
@@ -1,12 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Block : MonoBehaviour
 {
+    public float score;
+    //[SerializeField] public Text scoreText;
+
+     void Start()
+    {
+        score = 0;
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        DestroyBlock();
         Debug.Log(collision.gameObject.name); //What collided with the object? Debug purposes
+    }
+
+    private void DestroyBlock()
+    {
+        FindObjectOfType<ScoreManager>().addScore();
+        Destroy(gameObject);
     }
 }
---
+++ Arkanoid Game/Assets/Prefabs/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public float score;
+    public float blockDestroyed = 50f;
+    [SerializeField] public Text scoreText;
+
+    void Start()
+    {
+        score = 0;
+        SetScoreText();
+    }
+
+    public void addScore()
+    {
+        score += blockDestroyed;
+        SetScoreText();
+    }
+
+    public void SetScoreText() //Keeping score method
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
