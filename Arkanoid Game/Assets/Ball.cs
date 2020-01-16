d91640e4ea095f215eb9d32229bafcdc4838e4fd
DanielPinnington <danielpinnington12@gmail.com>
2020-01-16 11:40:19 +0000

    Sound clip implementation
    Sound clip implemented on collision (ball object)
    
    Array has been implemented to randomly choose an AudioClip to prevent repetition
    
    Possible bugs: When block object breaks the audioclip may also be deleted.

--- Arkanoid Game/Assets/Ball.cs
+++ Arkanoid Game/Assets/Ball.cs
@@ -8,16 +8,19 @@ public class Ball : MonoBehaviour
     [SerializeField] Paddle paddle1; //Type Paddle
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
-
+    [SerializeField] AudioClip[] ballSounds; //Array of audioclips
+    
     //State
-
     Vector2 paddleToBallVector;
-
     bool isLaunched = false;
+
+    //Cached component reference
+    AudioSource myAudioSource;
 
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -44,6 +47,19 @@ public class Ball : MonoBehaviour
     {
         Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePosition + paddleToBallVector;  //Ball Position is the same as Paddle positioning updated every frame
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isLaunched)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
+        else
+        {
+
+        }
     }
 
 }
