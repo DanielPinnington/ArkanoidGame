88794dcbec9d56ef5f1fbc76ac621bd8707d0e35
DanielPinnington <danielpinnington12@gmail.com>
2020-01-21 00:04:51 +0000

    Final Level nextScene
    When final level is completed, the correct scene (win scene) is displayed.

--- Arkanoid Game/Assets/Scripts/LevelManager.cs
+++ Arkanoid Game/Assets/Scripts/LevelManager.cs
@@ -24,7 +24,15 @@ public class LevelManager : MonoBehaviour
     public void LoadNextScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex + 1);
+
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Final Level")) //If the current scene equals (final level) then load Win scene
+        {
+            SceneManager.LoadScene("Start Menu"); //Change this to YOU WIN! scene
+        }
+        else
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1); //Else load next scene on building hierachy
+        }
     }
 
 
